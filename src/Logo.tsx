72045ef6fb84b9bb3a1dd2fe25be757cbbf0f701
Arman karimi <arman.ui.dev@gmail.com>
2026-02-07 01:17:35 +0330

    style: desktop with responsive padding

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -2,7 +2,8 @@ import img from "/images/logo.svg";
 
 const Logo = () => {
 	return (
-		<div className="flex items-center p-10">
+		<div className="flex items-center p-10 
+			 xl:pt-[10vh] xl:px-[25%]">
 			<img className="" src={img} alt="logo" />
 		</div>
 	);
