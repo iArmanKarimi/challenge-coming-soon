0604da4be416c71f20533b4814da3f3056d95770
Arman karimi <arman.ui.dev@gmail.com>
2026-02-07 01:20:45 +0330

    Fix: grid rows and cols

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import HeroImage from "./HeroImage";
 function App() {
 	return (
 		<div
-			className="bg-(image:--gradient-light) h-screen 
-			xl:grid xl:grid-cols-2 xl:grid-rows-2 xl:gap-0"
+			className="bg-(image:--gradient-light) h-screen xl:items-center
+			xl:grid xl:grid-cols-[2fr_1fr] xl:grid-rows-[auto_1fr] xl:gap-0"
 		>
 			<Logo />
 			<HeroImage />
