d27422b19ff8c02187be2745d9fabc76bec4f848
Arman karimi <arman.ui.dev@gmail.com>
2026-02-07 01:30:14 +0330

    Fix: bg covers whole screen not just current
    bg used to cover only screen height, not the whole scrollable height.

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import HeroImage from "./HeroImage";
 function App() {
 	return (
 		<div
-			className="bg-(image:--gradient-light) h-screen xl:items-center
+			className="bg-(image:--gradient-light) h-screen 
+			xl:items-center xl:overflow-auto
 			xl:grid xl:grid-cols-[2fr_1fr] xl:grid-rows-[auto_1fr] xl:gap-0"
 		>
 			<Logo />
