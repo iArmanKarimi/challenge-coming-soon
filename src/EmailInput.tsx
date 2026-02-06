281bf6290d3e281bff30d3b5190ae0bba24aa810
Arman karimi <arman.ui.dev@gmail.com>
2026-02-07 02:25:53 +0330

    feat: email validation + styles

--- src/EmailInput.tsx
+++ src/EmailInput.tsx
@@ -1,20 +1,44 @@
+import { useState } from "react";
 import SearchButton from "./SearchButton";
+import errIcon from "/images/icon-error.svg";
 
 const EmailInput = () => {
+	const [invalidEmail, setInvalidEmail] = useState(false);
+	const [email, setEmail] = useState("");
+
+	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
 	return (
-		<div
-			className="border-primary-pink 
+		<>
+			<div
+				className={`${invalidEmail ? "border-primary-red" : "border-primary-pink"}
 			border rounded-full h-15 w-full flex
-			focus-within:border-2 focus-within:border-primary-red"
-		>
-			<input
-				type="email"
-				placeholder="Email Address"
-				className="text-lg text-primary-pink border-none flex-1 px-6
-				focus:ring-0 focus:border-none focus:outline-none"
-			/>
-			<SearchButton />
-		</div>
+			focus-within:border-2 `}
+			>
+				<input
+					type="email"
+					placeholder="Email Address"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					onBlur={() => setInvalidEmail(!emailRegex.test(email))}
+					className="text-lg text-primary-pink border-none flex-1 px-6
+						focus:ring-0 focus:border-none focus:outline-none"
+				/>
+				<img
+					src={errIcon}
+					alt="Error"
+					className={`${invalidEmail ? "block" : "hidden"}
+						self-center mr-4 h-[50%]`}
+				/>
+				<SearchButton />
+			</div>
+			<p
+				className={`${invalidEmail ? "block" : "hidden"} 
+					text-primary-red text-sm mt-2 ml-6`}
+			>
+				Please provide a valid email
+			</p>
+		</>
 	);
 };
 
