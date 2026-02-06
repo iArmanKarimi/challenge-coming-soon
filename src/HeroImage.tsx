9fe0de8e1981f225d021b408083723abd25a8435
Arman karimi <arman.ui.dev@gmail.com>
2026-02-06 23:52:49 +0330

    react: set hero image on vw resize

--- src/HeroImage.tsx
+++ src/HeroImage.tsx
@@ -1,12 +1,23 @@
+import { useState, useEffect } from "react";
 import mobileHero from "/images/hero-mobile.jpg";
+import desktopHero from "/images/hero-desktop.jpg";
 
 const HeroImage = () => {
+	const [img, setImg] = useState<string>(mobileHero);
+
+	useEffect(() => {
+		const pickImg = () =>
+			window.innerWidth >= 1280 ? setImg(desktopHero) : setImg(mobileHero);
+		window.addEventListener("resize", pickImg);
+		return () => window.removeEventListener("resize", pickImg);
+	}, []);
+
 	return (
 		<img
-			src={mobileHero}
+			src={img}
 			alt="Hero"
 			className="w-full max-h-100 object-cover object-top 
-			xl:row-span-2"
+			xl:row-span-2 xl:max-h-screen xl:h-screen xl:object-center"
 		/>
 	);
 };
